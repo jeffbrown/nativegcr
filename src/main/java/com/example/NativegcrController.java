374b8dfc066e72c47cd443104708a73aa8648c1f
Jeff Scott Brown <brownj@objectcomputing.com>
2021-06-01 15:58:40 +0000

    Initial commit

---
+++ src/main/java/com/example/NativegcrController.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/nativegcr")
+public class NativegcrController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
